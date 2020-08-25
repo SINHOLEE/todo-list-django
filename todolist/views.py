6ea14ac389bcac5a82362406ac1a850aa53390b1
sinho <dltlsgh5@gmail.com>
2020-08-25 16:14:26 +0900

    refactor: change return value to render

--- todolist/views.py
+++ todolist/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<html><title>Todo List</title></html>')
-    # return render(request, 'todolist/index.html')
+    return render(request, 'todolist/index.html')
